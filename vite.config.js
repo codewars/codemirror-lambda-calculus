f2e05f548f14fdda9cd26245d0fd0ef4750eb699
kazk <kazk.dev@gmail.com>
2022-02-11 23:47:47 -0800

    Add evaluation

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+import WindiCSS from "vite-plugin-windicss";
+
 import { defineConfig } from "vite";
 import postcss from "./postcss.config.js";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/codemirror-lambda-calculus" : "",
+  base: mode === "production" ? "/codemirror-lambda-calculus/" : "",
+  plugins: [WindiCSS()],
   css: { postcss },
 }));
